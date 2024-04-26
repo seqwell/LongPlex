0057b113577d77420fa5040689547a18f5bd9e2c
seqwellBioinfo <156371200+seqwellBioinfo@users.noreply.github.com>
2024-04-26 07:09:30 -0400

    Update create_bbduk_summary.R
    update bbduk_summary in R file

--- create_bbduk_summary.R
+++ create_bbduk_summary.R
@@ -1,5 +1,10 @@
 #!/usr/bin/env Rscript
 library(dplyr)
+library(purrr)
+install.packages("openxlsx")
+library(openxlsx)
+
+
 args = commandArgs(trailingOnly=TRUE)
 
 output_name = args[[1]]
@@ -94,5 +99,46 @@ final_report = final_report %>%
 final_report
 bbduk_report
 
-readr::write_csv(bbduk_report, paste0(output_name, "_BBDuk_detailed_report.csv"))
-readr::write_csv(final_report, paste0(output_name, "_BBDuk_summary_report.csv"))
+lima_i5_file = list.files( full.names = T, pattern = "i5")
+i5_lima = readr::read_tsv( lima_i5_file )
+
+lima_i7_file = list.files( full.names = T, pattern = "i7")
+i7_lima = readr::read_tsv( lima_i7_file )
+names(i7_lima)
+
+count_file = list.files( full.names = T, pattern = "hifi.reads.count")
+all = readr::read_tsv( count_file, col_names = F)
+
+
+df= dplyr::bind_rows( i7_lima, i5_lima) %>% 
+  dplyr::select(IdxFirstNamed, Counts )
+
+df
+
+names(all) = c("IdxFirstNamed", "Counts")
+all$IdxFirstNamed[[1]] = "total"
+
+
+
+unbarcode = all$Counts-sum(df$Counts)
+unbarcode_pct = 100*unbarcode/all$Counts
+unbarcode_df = data.frame( IdxFirstNamed = c("unbarcoded", "unbarcoded_pct"), 
+                           Counts = c(unbarcode,unbarcode_pct)  , 
+                           stringsAsFactors = F )
+
+
+df_report = dplyr::bind_rows(df, all,unbarcode_df)
+
+df_report$Counts[-1] = round(df_report$Counts[-1], 0)
+
+#readr::write_csv(bbduk_report, paste0(output_name, "_BBDuk_detailed_report.csv"))
+#readr::write_csv(final_report, paste0(output_name, "_BBDuk_summary_report.csv"))
+
+
+#define sheet names for each data frame
+dataset_names <- list('lima_report' = df_report,
+                      'bbduk_detail' = bbduk_report,
+                      'bbduk_summary' = final_report)
+
+#export each data frame to separate sheets in same Excel file
+openxlsx::write.xlsx(dataset_names, file = paste0( pair_id, "_demux_report.xlsx"))
