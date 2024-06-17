d5627e72f3a97a501abff246775e7088ef71b479
seqwellBioinfo <156371200+seqwellBioinfo@users.noreply.github.com>
2024-06-17 16:59:44 -0400

    Update nextflow.sh

--- nextflow.sh
+++ nextflow.sh
@@ -5,7 +5,7 @@ samplesheet="samplesheet.csv"
 
 
 nextflow run \
-nextflow-pacbio-demux-bbduk-summary/pacbio_demux_bbduk_DSL2.nf \
+nextflow-pacbio-demux-bbduk-summary/pacbio_demux_bbduk.nf \
 -c nextflow-pacbio-demux-bbduk-summary/nextflow.config \
 --samplesheet $samplesheet \
 -bg -resume
