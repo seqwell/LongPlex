1e9f7f77cf085077d884c659e2cde68f1c81e4c7
seqwellBioinfo <156371200+seqwellBioinfo@users.noreply.github.com>
2024-06-18 16:33:00 -0400

    Update nextflow.sh
    add reports

--- nextflow.sh
+++ nextflow.sh
@@ -8,4 +8,5 @@ nextflow run \
 nextflow-pacbio-demux-bbduk-summary/pacbio_demux_bbduk.nf \
 -c nextflow-pacbio-demux-bbduk-summary/nextflow.config \
 --samplesheet $samplesheet \
+-with-report \
 -bg -resume
