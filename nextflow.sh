7ccd32c06bc1c364a62a9ea6b7a5eb776bd3e61c
seqwellBioinfo <156371200+seqwellBioinfo@users.noreply.github.com>
2024-07-02 12:15:12 -0400

    Update nextflow.sh

--- nextflow.sh
+++ nextflow.sh
@@ -2,11 +2,14 @@
 
 
 samplesheet="samplesheet/samplesheet.csv"
-
+outdir="output/LongPlex_demux_out"
 
 nextflow run \
+-profile singularity \
 nextflow-pacbio-demux-bbduk-summary/pacbio_demux_bbduk.nf \
 -c nextflow-pacbio-demux-bbduk-summary/nextflow.config \
 --samplesheet $samplesheet \
+--outdir  $outdir \
 -with-report \
+-with-trace  \
 -bg -resume
