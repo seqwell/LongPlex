4d8fb9cd76ecf81852d30e871b2c46f175380152
znorgaard <zach@fulcrumgenomics.com>
2024-09-25 15:24:09 -0700

    feat: add integration test

---
+++ tests/run_longplex_integration.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+nextflow run \
+    -profile docker \
+    -c "${PWD}/nextflow.config" \
+    main.nf \
+    --samplesheet "${PWD}/tests/samplesheet.csv" \
+    --output  "./output/"
