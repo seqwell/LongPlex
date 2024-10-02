1b35233310192087a9d55a19e2ad4777ed371814
znorgaard <zach@fulcrumgenomics.com>
2024-10-02 08:46:03 -0700

    feat: use latch samplesheet

--- latch_metadata/parameters.py
+++ latch_metadata/parameters.py
@@ -2,6 +2,8 @@
 from dataclasses import dataclass
 import typing
 import typing_extensions
+from pathlib import Path
+import csv
 
 from flytekit.core.annotation import FlyteAnnotation
 
@@ -9,22 +11,43 @@ from latch.types.metadata import NextflowParameter
 from latch.types.file import LatchFile
 from latch.types.directory import LatchDir, LatchOutputDir
 
+SAMPLE_ID: str = "sample_ID"
+SAMPLE_PATH: str = "sample_path"
+I7_PATH: str = "i7_barcode"
+I5_PATH: str = "i5_barcode"
+
+@dataclass(frozen=True)
+class Sample:
+    """Describes a seqWell Longplex sequencing pool
+
+    Attributes:
+        sample_ID: Name of the sequencing pool
+        sample_path: Path to unmapped BAM file
+        i7_barcode: Path to i7 barcodes in fasta format
+        i5_barcode: Path to i5 barcodes in fasta format 
+    """
+    sample_ID: str
+    sample_path: LatchFile
+    i7_barcode: LatchFile
+    i5_barcode: LatchFile
+
 # Import these into your `__init__.py` file:
 #
 # from .parameters import generated_parameters
 
 generated_parameters = {
-    'samplesheet': NextflowParameter(
-        type=LatchFile,
-        default=None,
-        section_tile='Input & Output Options',
-        description='CSV file containing samples with headers: sample_ID, sample_path, i7_barcode, i5_barcode',
+    "samplesheet": NextflowParameter(
+        type=typing.List[Sample],
+        samplesheet=True,
+        samplesheet_type="csv",
+        section_tile="Input & Output Options",
+        description="CSV file containing samples with headers: sample_ID, sample_path, i7_barcode, i5_barcode",
     ),
-    'output': NextflowParameter(
+    "output": NextflowParameter(
         type=LatchOutputDir,
         default=None,
-        section_tile='Input & Output Options',
-        description='Output directory',
+        section_tile="Input & Output Options",
+        description="Output directory",
     )
 }
 
