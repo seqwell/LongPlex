d5d58dcd5f2953074140d3fe68e162c18d6517ef
znorgaard <zach@fulcrumgenomics.com>
2024-10-02 10:42:16 -0700

    chore: add latch generated files

---
+++ wf/entrypoint.py
@@ -0,0 +1,188 @@
+import sys
+from dataclasses import dataclass
+from enum import Enum
+import os
+import subprocess
+import requests
+import shutil
+from pathlib import Path
+import typing
+import typing_extensions
+
+from latch.resources.workflow import workflow
+from latch.resources.tasks import nextflow_runtime_task, custom_task
+from latch.types.file import LatchFile
+from latch.types.directory import LatchDir, LatchOutputDir
+from latch.ldata.path import LPath
+from latch.executions import report_nextflow_used_storage
+from latch_cli.nextflow.workflow import get_flag
+from latch_cli.nextflow.utils import _get_execution_name
+from latch_cli.utils import urljoins
+from latch.types import metadata
+from flytekit.core.annotation import FlyteAnnotation
+
+from latch_cli.services.register.utils import import_module_by_path
+
+meta = Path("latch_metadata") / "__init__.py"
+import_module_by_path(meta)
+import latch_metadata
+
+@custom_task(cpu=0.25, memory=0.5, storage_gib=1)
+def initialize() -> str:
+    token = os.environ.get("FLYTE_INTERNAL_EXECUTION_ID")
+    if token is None:
+        raise RuntimeError("failed to get execution token")
+
+    headers = {"Authorization": f"Latch-Execution-Token {token}"}
+
+    print("Provisioning shared storage volume... ", end="")
+    resp = requests.post(
+        "http://nf-dispatcher-service.flyte.svc.cluster.local/provision-storage-ofs",
+        headers=headers,
+        json={
+            "storage_expiration_hours": 0,
+            "version": 2,
+        },
+    )
+    resp.raise_for_status()
+    print("Done.")
+
+    return resp.json()["name"]
+
+
+@dataclass
+class Sample:
+    sample_ID: str
+    sample_path: LatchFile
+    i7_barcode: LatchFile
+    i5_barcode: LatchFile
+
+
+
+
+samplesheet_construct_samplesheet = metadata._nextflow_metadata.parameters['samplesheet'].samplesheet_constructor
+
+
+@nextflow_runtime_task(cpu=4, memory=8, storage_gib=100)
+def nextflow_runtime(pvc_name: str, samplesheet: typing.List[Sample], output: typing_extensions.Annotated[LatchDir, FlyteAnnotation({'output': True})]) -> None:
+    shared_dir = Path("/nf-workdir")
+
+
+    samplesheet_samplesheet = samplesheet_construct_samplesheet(samplesheet)
+
+    ignore_list = [
+        "latch",
+        ".latch",
+        ".git",
+        "nextflow",
+        ".nextflow",
+        "work",
+        "results",
+        "miniconda",
+        "anaconda3",
+        "mambaforge",
+    ]
+
+    shutil.copytree(
+        Path("/root"),
+        shared_dir,
+        ignore=lambda src, names: ignore_list,
+        ignore_dangling_symlinks=True,
+        dirs_exist_ok=True,
+    )
+
+    profile_list = ['docker']
+    if False:
+        profile_list.extend([p.value for p in execution_profiles])
+
+    if len(profile_list) == 0:
+        profile_list.append("standard")
+
+    profiles = ','.join(profile_list)
+
+    cmd = [
+        "/root/nextflow",
+        "run",
+        str(shared_dir / "main.nf"),
+        "-work-dir",
+        str(shared_dir),
+        "-profile",
+        profiles,
+        "-c",
+        "latch.config",
+        "-resume",
+        *get_flag('samplesheet', samplesheet_samplesheet),
+                *get_flag('output', output)
+    ]
+
+    print("Launching Nextflow Runtime")
+    print(' '.join(cmd))
+    print(flush=True)
+
+    failed = False
+    try:
+        env = {
+            **os.environ,
+            "NXF_ANSI_LOG": "false",
+            "NXF_HOME": "/root/.nextflow",
+            "NXF_OPTS": "-Xms1536M -Xmx6144M -XX:ActiveProcessorCount=4",
+            "NXF_DISABLE_CHECK_LATEST": "true",
+            "NXF_ENABLE_VIRTUAL_THREADS": "false",
+        }
+        subprocess.run(
+            cmd,
+            env=env,
+            check=True,
+            cwd=str(shared_dir),
+        )
+    except subprocess.CalledProcessError:
+        failed = True
+    finally:
+        print()
+
+        nextflow_log = shared_dir / ".nextflow.log"
+        if nextflow_log.exists():
+            name = _get_execution_name()
+            if name is None:
+                print("Skipping logs upload, failed to get execution name")
+            else:
+                remote = LPath(urljoins("latch:///your_log_dir/nf_seqwell_longplex_demux", name, "nextflow.log"))
+                print(f"Uploading .nextflow.log to {remote.path}")
+                remote.upload_from(nextflow_log)
+
+        print("Computing size of workdir... ", end="")
+        try:
+            result = subprocess.run(
+                ['du', '-sb', str(shared_dir)],
+                check=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+                timeout=5 * 60
+            )
+
+            size = int(result.stdout.split()[0])
+            report_nextflow_used_storage(size)
+            print(f"Done. Workdir size: {size / 1024 / 1024 / 1024: .2f} GiB")
+        except subprocess.TimeoutExpired:
+            print("Failed to compute storage size: Operation timed out after 5 minutes.")
+        except subprocess.CalledProcessError as e:
+            print(f"Failed to compute storage size: {e.stderr}")
+        except Exception as e:
+            print(f"Failed to compute storage size: {e}")
+
+    if failed:
+        sys.exit(1)
+
+
+@workflow(metadata._nextflow_metadata)
+def nf_seqwell_longplex_demux(samplesheet: typing.List[Sample], output: typing_extensions.Annotated[LatchDir, FlyteAnnotation({'output': True})]) -> None:
+    """
+    seqWell LongPlex Demux
+
+    Sample Description
+    """
+
+    pvc_name: str = initialize()
+    nextflow_runtime(pvc_name=pvc_name, samplesheet=samplesheet, output=output)
+
