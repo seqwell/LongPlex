64b4155ab018144d5b586b290071ede86f4f9fc5
yanyanliubio <yanyanliujs@gmail.com>
2026-08-26 13:35:25 -0400

    update test file

--- tests/run_longplex_integration.sh
+++ tests/run_longplex_integration.sh
@@ -4,6 +4,6 @@ nextflow run \
     -profile docker \
     -c "${PWD}/nextflow.config" \
     -c "${PWD}/tests/limit-resources.config" \
-    main_conda.nf \
+    main.nf \
     --pool_sheet "${PWD}/tests/pool_sheet.csv" \
     --output  "${PWD}/output/"
